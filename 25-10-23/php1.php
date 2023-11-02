7c3bda296f8aeedad921b7c6f7846b221ec1c5b0
MURTHY HS <pavanpavan9938@gmail.com>
2023-11-02 14:59:04 +0530

    php added

---
+++ 25-10-23/php1.php
@@ -0,0 +1,9 @@
+<?php
+$a="ehkjfhe";
+echo("$a");
+echo "<br>";
+$b=25;
+$c=37;
+$d=$b+$c;
+echo("$d");
+?>
---
+++ 26-10-23/com.php
@@ -0,0 +1,14 @@
+<?php
+$name="technerds";
+// $name1="technerdsregege";
+print("$name"."<br>");
+print("the string is <br>".$name."<br>");
+echo($name."<br>");
+echo"$name<br>";
+// print"technerds"."<br>".$name1;
+echo"$name"."<br>";
+echo"$name"."<br>";
+echo"$name"."<br>";
+echo"$name"."<br>";
+
+?>
